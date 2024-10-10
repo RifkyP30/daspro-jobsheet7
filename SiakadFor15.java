5a6a61f0e107968c1a0f0a5fb5e486a090d4db70
Rifky <rifkypraditya1025@gmail.com>
2024-10-10 13:59:13 +0700

    modifikasi file SiakadFor15

--- SiakadFor15.java
+++ SiakadFor15.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class SiakadFor15 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    double nilai, tertinggi, terendah;
+    double nilai, tertinggi, terendah, nilaiMinimal;
+    byte jmlLulus = 0, jmlTdkLulus = 0;
     tertinggi = 0;
     terendah = 100;
+    nilaiMinimal = 60;
 
     for (int i = 1; i <= 10; i++) {
       System.out.print("Masukkan nilai mahasiswa ke " + i + ": ");
@@ -16,9 +18,17 @@ public class SiakadFor15 {
       if (nilai < terendah) {
         terendah = nilai;
       }
+      if (nilai >= nilaiMinimal) {
+        jmlLulus++;
+      }
+      else {
+        jmlTdkLulus++;
+      }
     }
 
     System.out.println("Nilai tertinggi: " + tertinggi);
     System.out.println("Nilai terendah: " + terendah);
+    System.out.println("Jumlah mahasiswa yang lulus: " + jmlLulus);
+    System.out.println("Jumlah mahasiswa yang tidak lulus: " + jmlTdkLulus);
   }
 }
